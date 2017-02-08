4e2cb34f405f2e09e1462df47ee1dd88cf5ac5f5
Tim LaRou <Tim LaRou>
2017-02-07 21:50:23 -0600

    updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,143 +1,130 @@
- 
-      $(document).ready(function() {
+ $(document).ready(function() {
 
-      // var words =['pokemon','pikachu','charizard','bulbasaur', 'jigglypuff','squirtle','pidgey','snorlax','butterfree', 'beedrill', 'rattata', 'ekans', 'nidoking','clefary','vulpix','gloom','diglett','meowth','psyduck','abra'];
+  var pokemon = {
+    p1 :{
+      name: 'Bulbasaur',
+      type: 'Grass/Poison',
+      number: 1,
+      caught: false,
+    },
+    p2: {
+      name: 'Charmander',
+      type: 'Fire',
+      number: 4,
+      caught: false,
+    },
+    p3: {
+      name: 'Venusaur',
+      type: 'Grass/Poison',
+      number: 3,
+      caught: false,
+    }
+  }
+  // if not adding all pokemon, create array to contain all numbers
+  var curWord = [];
+  var curGuess = [];
+  var guessRemain;
+  var caughtCount = 0;
+  var lastWord = -1;
+  var curPokemon;
+  console.log( ObjectLength(pokemon) );
 
-      var pokemon = {
-        p1 :{
-          name: 'Bulbasaur',
-          type: 'Grass/Poison',
-          number: 1,
-          caught: false,
-        },
-        p2: {
-          name: 'Charmander',
-          type: 'Fire',
-          number: 4,
-          caught: false,
-        },
-        p3: {
-          name: 'Venusaur',
-          type: 'Grass/Poison',
-          number: 3,
-          caught: false,
-        }
-      }
-      // if not adding all pokemon, create array to contain all numbers
-      var curWord = [];
-      var curGuess = [];;
-      var guessRemain;
-      var caughtCount = 0;
-      var lastWord = -1;
-      var curPokemon;
-      // Array(words.length).join("_");
-      // console.log( _.size(obj.Data) );
-        console.log( ObjectLength(pokemon) );
-      newGame();
-
+  newGame();
+//main - handles if there are button clicks
   document.onkeyup = function(event) {
 
-        // Captures the key press, converts it to lowercase, and saves it to a variable.
-        var letter = String.fromCharCode(event.keyCode).toLowerCase();
-        //index to determine if there are matches can probably replace this
-        var guess = curWord.indexOf(letter);
-        var guessCap = curWord.indexOf(letter.toUpperCase());
-        console.log(guessCap);
- 
+    var letter = String.fromCharCode(event.keyCode).toLowerCase();
+    var guess = curWord.indexOf(letter);
+    var guessCap = curWord.indexOf(letter.toUpperCase());
+    // console.log(guessCap);
 
-        if (guess != -1 || guessCap != -1) {
-          var allIndex = getAllIndexes(curWord,letter);
-          var allCapIndex = getAllIndexes(curWord,letter.toUpperCase());
+    if (guess != -1 || guessCap != -1) {
+      var allIndex = getAllIndexes(curWord,letter);
+      var allCapIndex = getAllIndexes(curWord,letter.toUpperCase());
 
-            for (var i = 0; i < allIndex.length; i++) {
-              curGuess[allIndex[i]] = letter;
-            }
-
-             for (var i = 0; i < allCapIndex.length; i++) {
-              curGuess[allCapIndex[i]] = letter.toUpperCase();
-            }
-
-
-          // curGuess[guess] = letter;
-          // curWord.push(words[i]);
-          $('#wordSpot').html(curGuess)
-          checkWin();
-
-        }
-        else {
-          guessRemain--;
-          $("#guessesRemaining").html(guessRemain);
-        }
-      };
-
-
-    function checkWin() {
-      var emptyLetter = curGuess.indexOf("_");
-      if (emptyLetter == -1) {
-        // alert('Winner Winner Chicken Dinner');
-        caughtCount++;
-        $("#totalCaught").html(caughtCount);
-        pokemon[curPokemon].caught = true;
-        newGame();
+      for (var i = 0; i < allIndex.length; i++) {
+        curGuess[allIndex[i]] = letter;
       }
+
+       for (var i = 0; i < allCapIndex.length; i++) {
+        curGuess[allCapIndex[i]] = letter.toUpperCase();
+      }
+
+      $('#wordSpot').html(curGuess)
+      checkWin();
     }
 
-      function newGame() {
+    else {
+      guessRemain--;
+      $("#guessesRemaining").html(guessRemain);
+    }
+  };
 
-        // var randomWord = Math.floor(Math.random()*20)+1;
+// checks to see if all letters have been guessed
+  function checkWin() {
+    var emptyLetter = curGuess.indexOf("_");
+    if (emptyLetter == -1) {
+      // alert('Winner Winner Chicken Dinner');
+      caughtCount++;
+      $("#totalCaught").html(caughtCount);
+      pokemon[curPokemon].caught = true;
+      newGame();
+    }
+  }
 
-        var randomPokemon = Math.floor(Math.random()*3)+1;
-        console.log(randomPokemon);
-        curPokemon = 'p' + randomPokemon;
-        curWord = [];
-        curGuess =[];
-        guessRemain = 10;
-        
-        var check = pokemon[curPokemon].caught;
-        
-        if (check) {
-          console.log('xxxx');
-          if (caughtCount != ObjectLength(pokemon)) {
-            newGame();
-          }
-          else {alert("Caught them All!");}
-        }
-        else {
-          
-          curGuess =[];
-          // var curName = 
-          curWord = pokemon[curPokemon].name.split("");
-          for (var i = 0; i < curWord.length; i++) {
-            curGuess.push("_");
-          }
-          $('#wordSpot').html(curGuess)
-          $("#guessesRemaining").html(guessRemain);
-        }
+//creates new game and updates screen
+  function newGame() {
+
+    // var randomWord = Math.floor(Math.random()*20)+1;
+
+    var randomPokemon = Math.floor(Math.random()*3)+1;
+    console.log(randomPokemon);
+    curPokemon = 'p' + randomPokemon;
+    curWord = [];
+    curGuess = [];
+    guessRemain = 10;
+    
+    var check = pokemon[curPokemon].caught;
+    
+    if (check) {
+      console.log('xxxx');
+      if (caughtCount != ObjectLength(pokemon)) {
+        newGame();
       }
-
-    function getAllIndexes(arr, val) {
-          var indexes = [], i;
-          for(i = 0; i < arr.length; i++)
-              if (arr[i] === val)
-                  indexes.push(i);
-          return indexes;
+      else {alert("Caught them All!");}
+    }
+    else {
+      
+      curGuess =[];
+      // var curName = 
+      curWord = pokemon[curPokemon].name.split("");
+      for (var i = 0; i < curWord.length; i++) {
+        curGuess.push("_");
       }
-
+      $('#wordSpot').html(curGuess)
+      $("#guessesRemaining").html(guessRemain);
+    }
+  }
+// handler for finding all matches
+  function getAllIndexes(arr, val) {
+    var indexes = [], i;
+    for(i = 0; i < arr.length; i++)
+      if (arr[i] === val)
+        indexes.push(i);
+    return indexes;
+  }
+//determines how many pokemon are in the array to check if they are all caught (could probably hardcode)
   function ObjectLength( object ) {
     var length = 0;
     for( var key in object ) {
-        if( object.hasOwnProperty(key) ) {
-            ++length;
-        }
+      if( object.hasOwnProperty(key) ) {
+        ++length;
+      }
     }
     return length;
-    };
+  };
 
-
-
-
-
-    });
+}); //end of document ready
 
 
  
